0d4789196253df9aeb95808a31143f052a951263
kazyna95 <54522756+kazyna95@users.noreply.github.com>
2019-09-27 17:30:34 +0600

    Add files via upload

---
+++ voucher-main.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'voucher-main',
+    templateUrl: './voucher-main.page.html',
+    styleUrls: ['./voucher-main.page.scss']
+})
+export class VoucherMainPageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
